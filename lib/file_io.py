e148b211c8e7dd2940c9977b81fddb89bfe89ad2
Feasibo <feasibo@gmail.com>
2025-04-12 23:09:36 +0300

    commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    """Creates or overwrites a .txt file with the provided content"""
+    with open(f'{file_name}.txt', "w") as file: 
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
-
+def append_file(file_name, file_content):
+    """Append the provided contnet to a .txt file. If the file does not exist, it will be cvreated"""
+    with open(f'{file_name}.txt', "a") as file:
+        file.write(file_content)
 def read_file(file_name):
-    pass
+    """Reads and returns the content of a .text file."""
+    with open (f"{file_name}.txt", "r") as file:
+        return file.read()
